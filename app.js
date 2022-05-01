009afede4b392542d50e98707abcc3591be4753e
Rambus <jordaan.rambus@capgemini.com>
2022-05-01 18:56:34 -0500

    variable Port

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.get('/',(req,res)=> {
     res.send("My first Node Js App home page");
 });
 
-
-app.listen(3001, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
  console.log("ahhh so you are so you are in the console ehhh");
 });
 
